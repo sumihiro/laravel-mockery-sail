4b4d3f38b12a6742ad82b2f70d319b5b4d97766c
sumihiro <ueda@sumihiro.biz>
2022-12-16 09:46:09 +0900

    Mockeryを使ってHelloWorldServiceをモックしたテストを追加

---
+++ api-sample/tests/Feature/MockedEchoTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Services\HelloWorldService;
+use Mockery;
+use Tests\TestCase;
+
+class MockedEchoTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_echo_mockしたapiを叩いてByeWorldメッセージが返ってくるか()
+    {
+        // 実際の予約処理をモックしておく
+        $reserveMock = Mockery::mock('overload:' . HelloWorldService::class);
+        $reserveMock->shouldReceive('hello')->andReturn('Bye World...');
+        
+        $response = $this->get('/api/echo');
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'message' => 'Bye World...'
+        ]);
+    }
+}
